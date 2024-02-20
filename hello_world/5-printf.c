483cb5053b7cea7201294e1efc8697385caaa544
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-20 11:57:06 +0100

    Ecrivez un programme C qui imprime exactement avec une grammaire correcte, mais le résultat est une œuvre d'art,, suivie d'une nouvelle ligne. Utiliser la fonction printf Vous n'êtes pas autorisé à utiliser la fonction puts Votre programme doit retourner 0 Votre programme doit se compiler sans avertissement lors de l'utilisation de l'option -Wall de gcc

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - Code to put a message with printf
- * Return: Always 0 (Succes)
+ * main - texto
+ *
+ * Return: text in printf
  */
+
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
-}	  
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
