12503acd7e73a340461de3ec795bc1a5238f4c54
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-21 12:44:17 +0100

    Ecrivez un programme qui imprime tous les nombres à un chiffre de la base 10 à partir de 0, suivi d'une nouvelle ligne. Vous n'avez pas le droit d'utiliser une variable de type char Vous ne pouvez utiliser que la fonction putchar (toutes les autres fonctions (printf, puts, etc...) sont interdites) Vous ne pouvez utiliser putchar que deux fois dans votre code Tout votre code doit se trouver dans la fonction principale

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers of base 10 starting from 0,
+* followed by a new line.
+*
+* Return: Always 0 (Succes)
+*/
+int main(void)
+{
+	const char numbers(i) = '0', '1', '2', '3', '4', '5', '6', '7', '8', '9';
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+		putchar(numbers(i));
+	{
+	    putchar("\n");
+	}
+	return (0);
+}
