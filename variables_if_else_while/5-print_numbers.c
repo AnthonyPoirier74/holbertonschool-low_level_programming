b9199bbbaf0273f6710c0abcc8b2a1109ee37347
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-21 12:00:03 +0100

    changement réponse

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -12,12 +12,12 @@ int main(void)
 	for (x = 0; x < 10; x++)
 	{
 		printf("%d", x);
-		if (x < 9)
-		{
-			printf(", ");
-		}
+	if (x < 10)
+	{
+		printf(",x");
 	}
-	printf("\n");
+	}
+	    printf("\n");
 
 	return (0);
 }
