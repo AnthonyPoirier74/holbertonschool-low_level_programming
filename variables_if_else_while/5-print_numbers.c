41dc5b8fe717fe73858f643af5d2a9816bbce5c6
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-21 12:14:07 +0100

    Ecrivez un programme qui imprime tous les nombres à un chiffre de la base 10 à partir de 0, suivi d'une nouvelle ligne. Tout votre code doit se trouver dans la fonction principale

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-/*
- * main - Prints all single-digit base 10 numbers starting from 0,
- * followed by a new line.
- *
- * Return: Always 0 (Succes)
- */
+/**
+* main - Prints all single-digit base 10 numbers starting from 0,
+* followed by a new line.
+*
+* Return: Always 0 (Succes)
+*/
 int main(void)
 {
 	int x;
 
 	for (x = 0; x < 10; x++)
-	{
+
 		printf("%d", x);
-	if (x < 10)
-	{
-		printf(",x");
-	}
-	}
+
 	    printf("\n");
 
 	return (0);
