66a0ee2ddf300fddff8a21396e4f151507860ffc
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-21 10:51:42 +0100

    changement réponse

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Print the alphabet in lowercase,
- * in uppercase,  followed by a newline.
- * Only allowed to use putchar three times.
- *
- * Return: Always 0 (Success)
- */
+* main - Print the alphabet in lowercase,
+* in uppercase,  followed by a newline.
+* Only allowed to use putchar three times.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		putchar(c);
-
-	putchar('\n');
+	}
 
 	for (c = 'A'; c <= 'Z'; c++)
+	{
 		putchar(c);
+	}
 
 	putchar('\n');
 
