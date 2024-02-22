8c4c620888c7b68ab73f725ec62c1d78ffc15469
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-23 00:11:33 +0100

    changement reponse

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
 /**
-*  _putchar - Fonction for print a single character
-*             on standard output.
+* _putchar - Print a single character on standard output.
+* &a: The character to print.
+*
+* Return: On success, the number of bytes written (1) is returned.
+* On error, -1 is returned, and errno is set appropriately.
+*
 */
 int _putchar(char a)
 {
