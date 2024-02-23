54fcb74a697b6da1f3b866889a6ce26bb5670f12
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-23 18:14:04 +0100

    Écrire une fonction qui imprime le signe d'un nombre. Prototype : int print_sign(int n) ; Renvoie 1 et imprime + si n est supérieur à zéro Renvoie 0 et imprime 0 si n est égal à zéro Renvoie -1 et imprime - si n est inférieur à zéro

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,17 +9,21 @@
 *         -1 if n is less than zero.
 */
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-	return (-1);
+		_putchar('-');
+		return (-1);
 	}
 }
