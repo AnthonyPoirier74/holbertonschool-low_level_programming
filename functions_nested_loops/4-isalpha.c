a8fc57dedfb67942ac807720e98ee02fd5216922
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-23 16:14:10 +0100

    Écrire une fonction qui vérifie la présence d'un caractère alphabétique. Prototype : int _isalpha(int c) ; Renvoie 1 si c est une lettre, minuscule ou majuscule Retourne 0 dans le cas contraire

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+* _isalpha - Function that checks if a given character is an alphabetic letter.
+* @c: The character to be checked
+*
+* Return: Returns a nonzero value if the character is an alphabetic letter,
+*         0 otherwise.
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
