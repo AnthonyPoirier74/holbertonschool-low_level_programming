36f82b5ffec3d0e9bf986d77abf99d4a5052bc49
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-26 11:36:53 +0100

    Écrire une fonction qui vérifie la présence d'un chiffre (0 à 9).

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 int _isupper(int c);
@@ -12,3 +13,4 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+#endif
