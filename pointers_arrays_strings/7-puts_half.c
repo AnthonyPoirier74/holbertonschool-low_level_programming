108d4074d9a204a2549aa52a2a213f94c7ee0105
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-28 21:44:43 +0100

    Écrire une fonction qui imprime la moitié d'une chaîne de caractères, suivie d'une nouvelle ligne.

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string
+ * @str: The string to process
+ *
+ * Description: This function prints the second half of a string,
+ * followed by a newline character.
+ */
+void puts_half(char *str)
+{
+	int n, mid, length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		mid = length / 2;
+		for (n = mid; n < length; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		mid = (length - 1) / 2;
+		for (n = mid + 1; n < length; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
