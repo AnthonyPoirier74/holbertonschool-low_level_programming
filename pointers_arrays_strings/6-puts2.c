e634b3451e4ebb0609e61629be3a52d94e433ded
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-02-28 14:48:24 +0100

    answer 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Imprime chaque autre caractère d'une chaîne,
+ *         en commençant par le premier.
+ * @str: La chaîne de caractères à imprimer.
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a += 2;
+	}
+	_putchar('\n');
+}
