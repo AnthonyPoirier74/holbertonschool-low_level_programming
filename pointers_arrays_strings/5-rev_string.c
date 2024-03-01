0a8e9163d36022d4e44f2cf7fe9afc027bd02a3b
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-03-01 18:35:38 +0100

    Écrire une fonction qui inverse une chaîne de caractères.

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+ void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+	char firstChar;
+	char lastChar;
+
+	while (s[len] != '\0')
+	{
+		printf("%c", len);
+		len++;
+	}
+	len--;
+	for (; i <= len / 2; i++)
+	{
+		firstChar = s[i];
+		lastChar = s[len - i];
+
+		s[len - i] = firstChar;
+		s[i] = lastChar;
+		printf("%c", i);
+	}
+}
