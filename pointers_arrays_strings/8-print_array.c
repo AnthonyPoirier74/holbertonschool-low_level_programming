97f923551cebbff67b9e315dcdadbc3fc610f3fe
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-03-01 10:33:46 +0100

    push answer 8 - 9 and olds main.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - a
+*
+*@a : a
+*@n : a
+*/
+
+void print_array(int *a, int n)
+{
+	int b;
+
+	if (n < 0 || a == NULL)
+		return;
+
+	for (b = 0; b < n; b++)
+	{
+		printf("%d", a[b]);
+
+		if (b < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string.
+ * @dest: Destination buffer
+ * @src: Source string to be copied
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	dest[a] = '\0';
+
+	return (dest);
+}
