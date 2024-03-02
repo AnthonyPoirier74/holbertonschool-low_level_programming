760d7ebb8179243ece46f11ea5d74dd55e996315
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-03-02 08:27:11 +0100

    change answer 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,23 +10,16 @@ void rev_string(char *s)
 	int len = 0;
 	int i = 0;
 	char firstChar;
-	char lastChar;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 	len--;
-	printf("%s\n");
-
 	for (; i <= len / 2; i++)
 	{
 		firstChar = s[i];
-		lastChar = s[len - i];
-
+		s[i] = s[len - i];
 		s[len - i] = firstChar;
-		s[i] = lastChar;
 	}
-
-	printf("%s\n", s);
 }
