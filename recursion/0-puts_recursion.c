8c329b1ecc7f5fbea1431f3b43ff11e1a71a2ddd
AnthonyPoirier74 <anthonypoirier74@yahoo.fr>
2024-03-07 14:59:06 +0100

    change answer

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
+ * _puts_recursion - Prints a string followed by a newline using recursion
+ * @s: Pointer to the string to be printed
  *
- *
- *
- *
- *
- *
+ * Description: This function prints each character of the string pointed to by
+ * 's' followed by a newline character ('\n'). It utilizes recursion to
+ * achieve this.
  **/
 void _puts_recursion(char *s)
 {
